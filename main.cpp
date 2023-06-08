d6908a7968003e51e538e5845be92b646838cb86
xincheng-cao <36287551+xincheng-cao@users.noreply.github.com>
2023-06-08 21:03:19 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ class MyClass{
 public:
     static vector<int> myStaticVector;
     const static int myStaticNumber=1;
-    const vector<int> myStaticVector2= vector<int>(6,10000);
+    const vector<int> myConstVector2= vector<int>(6,10000);
+    vector<int> myVector3= vector<int>(6,10000);
 };
 
 vector<int> MyClass::myStaticVector= vector<int>(3,2);
@@ -15,7 +16,9 @@ vector<int> MyClass::myStaticVector= vector<int>(3,2);
 int main() {
     MyClass::myStaticVector.push_back(33);
     MyClass myClass=MyClass();
-    cout<<myClass.myStaticVector2[5]<<endl;
+    cout<<myClass.myConstVector2[5]<<endl;
+    myClass.myVector3.push_back(12312);
+    cout<<(*(myClass.myVector3.end()-1))<<endl;
 
     cout<<MyClass::myStaticNumber<<endl;
     std::cout << "Hello, World!" << std::endl;
